c70890912b8ade0b5a51da28d4f299d3e4e65bec
Aaron Coppock <arc.13@me.com>
2018-01-21 15:07:13 -0800

    Models for csproj file

---
+++ NetCoreStandardPackageUpdater/NetCoreStandardPackageUpdater/Models/ModelPropertyGroup.cs
@@ -0,0 +1,108 @@
+﻿using System.Xml.Serialization;
+
+namespace NetCoreStandardPackageUpdater.Models
+{
+    /// <summary>
+    /// Model class for Property Group
+    /// </summary>
+    /// <author>
+    /// Aaron Coppock
+    /// </author>
+    /// <date>
+    /// 1/21/2018
+    /// </date>
+    [XmlRoot(ElementName = "PropertyGroup")]
+    public class ModelPropertyGroup
+    {
+        [XmlElement(ElementName = "AppendTargetFrameworkToOutputPath")]
+        public string AppendTargetFrameworkToOutputPath
+        {
+            get;
+            set;
+        }
+
+        [XmlElement(ElementName = "TargetFramework")]
+        public string TargetFramework
+        {
+            get;
+            set;
+        }
+
+        [XmlElement(ElementName = "AssetTargetFallback")]
+        public string AssetTargetFallback
+        {
+            get;
+            set;
+        }
+
+        [XmlElement(ElementName = "GeneratePackageOnBuild")]
+        public string GeneratePackageOnBuild
+        {
+            get;
+            set;
+        }
+
+        [XmlElement(ElementName = "PackageId")]
+        public string PackageId
+        {
+            get;
+            set;
+        }
+
+        [XmlElement(ElementName = "Authors")]
+        public string Authors
+        {
+            get;
+            set;
+        }
+
+        [XmlElement(ElementName = "Company")]
+        public string Company
+        {
+            get;
+            set;
+        }
+
+        [XmlElement(ElementName = "PackageProjectUrl")]
+        public string PackageProjectUrl
+        {
+            get;
+            set;
+        }
+
+        [XmlElement(ElementName = "Description")]
+        public string Description
+        {
+            get;
+            set;
+        }
+
+        [XmlElement(ElementName = "Copyright")]
+        public string Copyright
+        {
+            get;
+            set;
+        }
+
+        [XmlElement(ElementName = "AssemblyVersion")]
+        public string AssemblyVersion
+        {
+            get;
+            set;
+        }
+
+        [XmlElement(ElementName = "FileVersion")]
+        public string FileVersion
+        {
+            get;
+            set;
+        }
+
+        [XmlElement(ElementName = "Version")]
+        public string Version
+        {
+            get;
+            set;
+        }
+    }
+}
---
+++ NetCoreStandardPackageUpdater/NetCoreStandardPackageUpdater/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace NetCoreStandardPackageUpdater
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Check for args
+        }
+    }
+}
